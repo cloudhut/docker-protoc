b8236f11284ed2bb5f927fd9e03a29b55a2f4a1e
Martin Schneppenheim <weeco91@gmail.com>
2020-03-09 22:16:16 +0100

    Rebuild

--- dist/index.js
+++ dist/index.js
@@ -64,23 +64,27 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = __importDefault(__webpack_require__(470));
+const core = __importStar(__webpack_require__(470));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            core_1.default.info('Starting buf action');
-            const runLint = core_1.default.getInput('runLint') == 'true' ? true : false;
+            core.info('Starting buf action');
+            const runLint = core.getInput('run-linter') == 'true' ? true : false;
             if (runLint) {
-                core_1.default.info('Running linter');
+                core.info('Running linter');
             }
-            core_1.default.info('Successfully ran buf');
+            core.info('Successfully ran buf');
         }
         catch (error) {
-            core_1.default.setFailed(error.message);
+            core.setFailed(error.message);
         }
     });
 }
